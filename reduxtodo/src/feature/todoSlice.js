6ad89d0d4b05820db66a502263647da87fc8eee9
Riten1 <122423532+Riten1@users.noreply.github.com>
2023-11-26 16:18:07 +0545

    'reducers'

--- reduxtodo/src/feature/todoSlice.js
+++ reduxtodo/src/feature/todoSlice.js
@@ -1,1 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{
+    id: 1,
+    text: 'Cooking'
+  }]
+}
+
+export const todoSlice = createSlice({
+  name: 'Todo',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload
+      }
+      state.todos.push(todo);
+    }, 
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+    }
+
+  }
+})
+
+export default todoSlice.reducer
+
+export const { addTodo, removeTodo } = todoSlice.actions
